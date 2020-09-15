167c307e2d74b81c5036e2baa7ad0f41e331c2f9
foo <foo@bar.de>
2020-09-15 18:08:20 +0200

    fix: set node version

--- src/index.js
+++ src/index.js
@@ -39,6 +39,7 @@ export default {
     '/firebase.json',
   ],
   lint,
+  nodeVersion: 10,
   prepare: async () => {
     await execa.command('yarn', { cwd: 'functions' })
     await copyFile('.babelrc.json', P.join('functions', '.babelrc.json'))
